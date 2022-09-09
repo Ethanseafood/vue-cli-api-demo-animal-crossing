96b53561588d74d4be1cdac6ac53460308ca786a
Ethan <ethangpe@gmail.com>
2022-09-09 14:54:40 +0800

    set  router

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand">Home</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/bike" class="nav-link">BikeList</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/fish" class="nav-link">FishList</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <router-view />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/BikeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      msg: "123",
+    };
+  },
+};
+</script>
