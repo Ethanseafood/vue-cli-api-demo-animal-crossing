cd50301e625f7cd92f78bb2a67310f6a15ce4ee8
Ethan <ethangpe@gmail.com>
2023-03-09 16:13:51 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+module.exports = defineConfig(
+  {
+    transpileDependencies: true,
+  },
+  {
+    publicPath:
+      process.env.NODE_ENV === "production"
+        ? "/tvue-cli-api-demo-animal-crossing/" 
+        : "/",
+  }
+);
